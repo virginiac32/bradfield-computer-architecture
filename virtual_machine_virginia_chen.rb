e86930b82a29dee257922b2389c23ac398e10fd2
Virginia Chen <virginia.chen32@gmail.com>
2018-12-13 08:16:56 -0800

    Update to virtual machine

--- virtual_machine_virginia_chen.rb
+++ virtual_machine_virginia_chen.rb
@@ -1,5 +1,3 @@
-# ADDRESSES = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13]
-
 LOAD_WORD = 0x01
 STORE_WORD = 0x02
 ADD = 0x03
@@ -9,46 +7,47 @@ GEN_REG_1 = 0x01
 GEN_REG_2 = 0x02
 PC = 0x00
 
-# do all additions/subtractions get stored in register 1?
-
-
 def virtual_computer(memory)
-  registers = [0, 0, 0]
+  registers = [0x0000, 0x0000, 0x0000]
 
   until memory[registers[PC]] == HALT
     case memory[registers[PC]]
     when LOAD_WORD
       registers[PC] += 1
-      register = registers[memory[registers[PC]]]
+      register_address = memory[registers[PC]]
       registers[PC] += 1
-      value = memory[registers[PC]]
+      memory_address = memory[registers[PC]]
+      value = memory[memory_address] + (memory[memory_address+1])*256
       registers[PC] += 1
-      registers[register] = value
+      registers[register_address] = value
     when STORE_WORD
       registers[PC] += 1
-      register = registers[memory[registers[PC]]]
+      register_value = registers[memory[registers[PC]]]
       registers[PC] += 1
-      address = memory[registers[PC]]
+      memory_address = memory[registers[PC]]
       registers[PC] += 1
-      memory[address] = registers[register]
+      memory[memory_address] = register_value % 256
+      memory[memory_address+1] = register_value / 256
     when ADD
       registers[PC] += 1
+      register_address = memory[registers[PC]]
       value_1 = registers[memory[registers[PC]]]
       registers[PC] += 1
       value_2 = registers[memory[registers[PC]]]
       registers[PC] += 1
-      memory[address] = registers[register]
-      registers[GEN_REG_1] = value_1 + value_2
+      registers[register_address] = value_1 + value_2
     when SUBTRACT
       registers[PC] += 1
+      register_address = memory[registers[PC]]
       value_1 = registers[memory[registers[PC]]]
       registers[PC] += 1
       value_2 = registers[memory[registers[PC]]]
       registers[PC] += 1
-      memory[address] = registers[register]
-      registers[GEN_REG_1] = value_1 - value_2
+      registers[register_address] = value_1 - value_2
     end
   end
+
+  p memory
 end
 
 # tests
@@ -64,3 +63,17 @@ memory = [
   0xa1, 0x14,
   0x0c, 0x00
 ]
+
+memory2 = [
+ 1,1,0x10, # 0x00: load A 0x10
+ 1,2,0x12, # 0x03: load B 0x12
+ 3,1,2,    # 0x06: add A B
+ 2,1,0x0e, # 0x09: store A 0x0E
+ 0xFF,     # 0x0C: halt
+ 0,        # 0x0D: <<unused>>
+ 0,0,      # 0x0E: output
+ 2,0,      # 0x10: input X = 2
+ 3,0       # 0x12: input Y = 3
+]
+
+virtual_computer(memory)
